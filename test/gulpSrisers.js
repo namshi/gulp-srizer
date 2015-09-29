37fb0e21edc311d9ae8d08d0381c9d3fe447d544
Luciano Colosio <luciano.colosio@namshimshi.com>
2015-09-29 11:27:42 +0400

    Reindent and adding test

--- test/gulpSrisers.js
+++ test/gulpSrisers.js
@@ -1,30 +1,48 @@
-var path = require('path');
-var through2 = require('through2');
-var gulp = require('gulp');
+
+var gutil = require('gulp-util');
+var fs = require('fs');
+var should = require('should');
 var sriResources = require('../index');
-var fixsturesPath = path.join(__dirname, 'fixtures');
 
-function checkResult(cb) {
-  var files = {};
-
-  return through2.obj(function(file, enc, callback) {
-    var filePath = path.relative(file.base, file.path);
-    files[filePath] = file.contents.toString();
-    callback(null, file);
-  }).on('finish', function() {
-    cb(files);
+function loadFile(name) {
+  return new gutil.File({
+    path: 'test/expected/' + name,
+    cwd: 'test/',
+    base: 'test/expected/',
+    contents: fs.readFileSync('test/expected/' + name)
   });
 }
 
+function processInput(opts, srcFile, expectedFileName, done) {
+  var stream = sriResources(opts);
+
+  stream.on('error', function(err) {
+    should.not.exist(err);
+    done(err);
+  });
+
+  stream.on('data', function(newFile) {
+    should.exist(newFile);
+    should.exist(newFile.contents);
+
+    String(newFile.contents).should.equal(String(loadFile(expectedFileName).contents));
+    done();
+  });
+
+  stream.write(srcFile);
+  stream.end();
+}
+
 describe('Srizer', function() {
-  it('Should add sris to css and js files', function(done) {
-    console.log(__dirname);
-    gulp.src('./fixtures/source/*.html')
-        .pipe(sriResources())
-        .pipe(gulp.dest('dist/blah')).on('end', done);
-        // .pipe(checkResult(function(files) {
-        //   console.log(files);
-        //   done();
-        // }));
+  it('Should add sris to css and js resources in html file', function(done) {
+
+    var srcFile = new gutil.File({
+      path: 'test/fixtures/source/index.html',
+      cwd: 'test/',
+      base: 'test/fixtures/source/',
+      contents: fs.readFileSync('test/fixtures/source/index.html')
+    });
+
+    processInput({}, srcFile, 'css+js.html', done);
   });
 });
